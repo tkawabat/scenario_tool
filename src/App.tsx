814a6d67428a093a66704cdc02e713ce1a6dfd4f
tkawabat <tosiki.kawabata@gmail.com>
2020-12-26 21:57:25 +0900

    PDFの処理を削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HashRouter, Switch, Route, } from 'react-router-dom';
 
 import Hakogaki from './view/l5/Hakogaki';
-import Pdf from './view/l5/Pdf';
 
 const App = () => {
 
@@ -10,9 +9,6 @@ const App = () => {
         <HashRouter>
             <Switch>
                 <Route path="/hakogaki" component={Hakogaki} />
-                <Route path="/pdf" component={Pdf}>
-
-                </Route>
             </Switch>
         </HashRouter>
     );
