2784780ef70bdb2f9220ea99af9ae89e34fc7f3f
tkawabat <tosiki.kawabata@gmail.com>
2021-09-24 22:05:21 +0900

    NotificationSlice消し忘れ

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit'
 
 import ScenarioSlice from './ScenarioSlice';
-import NotificationSlice from './NotificationSlice';
 
 const rootReducer = combineReducers({
     scenario: ScenarioSlice.reducer,
-    notification: NotificationSlice.reducer,
 })
  
 export type RootState = ReturnType<typeof rootReducer>
